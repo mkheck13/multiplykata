e568e82190579d6d015fd8a5d2caad1833bcdac0
mkheck13 <mkheck13@gmail.com>
2024-06-11 10:04:49 -0700

    multiplykata

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+//This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+    a * b
+  }
+
+
+// It was missing the return and semi colon
+// works now
+  function multiply(a, b){
+    return a * b;
+  } 
